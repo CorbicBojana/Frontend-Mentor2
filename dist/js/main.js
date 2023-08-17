2092f15106ce308cd2c7a7f214e7134ebeb44bc9
CorbicBojana <corbicbojana@gmail.com>
2023-08-17 17:48:58 +0200

    add slider mobile

--- dist/js/main.js
+++ dist/js/main.js
@@ -12,3 +12,23 @@ menuClose.addEventListener("click", () => {
     nav.classList.remove("show");
     menuClose.classList.remove("show");
 })
+
+//==========SLIDER
+let slideIndex = 1;
+
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.querySelectorAll(".slides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
