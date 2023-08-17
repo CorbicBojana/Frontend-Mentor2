25d827a9fd2f6adb2670a0ab4d0717def81d2c3c
CorbicBojana <corbicbojana@gmail.com>
2023-08-17 18:58:28 +0200

    add slider desktop

--- dist/js/main.js
+++ dist/js/main.js
@@ -22,13 +22,23 @@ function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
 function showSlides(n) {
   let i;
   let slides = document.querySelectorAll(".slides");
+  let thumbnail = document.querySelectorAll(".thumbnail");
+
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+  for (i = 0; i < thumbnail.length; i++) {
+    thumbnail[i].className = thumbnail[i].className.replace(" main_border", "");
+  }
   slides[slideIndex-1].style.display = "block";
+  thumbnail[slideIndex-1].className += " main_border";
 }
